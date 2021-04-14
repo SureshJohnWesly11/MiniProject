38c17cbc2e745fd0a33b52250bd028657c3aa95a
SureshJohnWesly11 <59410838+SureshJohnWesly11@users.noreply.github.com>
2021-04-15 02:06:07 +0530

    Update Test_Functionalities.c

--- 3_Implementation/test/Test_Functionalities.c
+++ 3_Implementation/test/Test_Functionalities.c
@@ -1,14 +1,9 @@
-//#include "unity.h"
+#include "unity.h"
 
 /* Modify these two lines according to the project */
 #include "Functionalities.h"
 #define PROJECT_NAME    "Train Ticket Reservation"
 
-/* Prototypes for all the test functions */
-void test_add(void);
-void test_subtract(void);
-void test_multiply(void);
-void test_divide(void);
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -22,40 +17,11 @@ int main()
   UNITY_BEGIN();
 
 /* Run Test functions */
-  RUN_TEST(test_add);
-  RUN_TEST(test_subtract);
-  RUN_TEST(test_multiply);
-  RUN_TEST(test_divide);
-
+  
   /* Close the Unity Test Framework */
   return UNITY_END();
 }
 
 /* Write all the test functions */ 
-void test_add(void) {
-  TEST_ASSERT_EQUAL(30, add(10, 20));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(15000, add(7500, 7500));
-}
 
-void test_subtract(void) {
-  TEST_ASSERT_EQUAL(-3, subtract(0, 3));
   
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(100, subtract(1000, 900));
-}
-
-void test_multiply(void) {
-  TEST_ASSERT_EQUAL(0, multiply(1, 0));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(10, multiply(2, 5));
-}
-
-void test_divide(void) {
-  TEST_ASSERT_EQUAL(0, divide(1, 0));
-  
-  /* Dummy fail*/
-  TEST_ASSERT_EQUAL(1, divide(2, 2));
-}
